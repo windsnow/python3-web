72e7a2b9a20c006e7d0bd5a4d57ee1bf4418c97d
sundshine <1326953471@qq.com>
2017-09-05 16:29:00 +0800

    base blog

--- www/app.py
+++ www/app.py
@@ -110,7 +110,8 @@ def datetime_filter(t):
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 async def init(loop):
-    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www', password='www', db='awesome')
+    # yield from orm.create_pool(loop=loop, **configs.db)
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='hy1505', db='awesome')
     app = web.Application(loop=loop, middlewares=[
         logger_factory, response_factory
     ])
